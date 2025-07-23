ca4dafa340eb9867175a8122d6185d419c9f9bb5
Jonathan <jonathan.mfm@gmail.com>
2025-07-23 10:52:10 +0200

    feat: recuperation des fleches cote JS et ajout de l'ecoute du clic

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,23 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+//=========//
+// Étape 2 //
+//=========//
+
+// Récupère l'element avec la classe ".arrow_left" pour le stocker dans la constante arrowLeft
+const arrowLeft = document.querySelector(".arrow_left")
+
+// 	Idem avec arrow_right
+const arrowRight = document.querySelector(".arrow_right")
+
+// Sur arrowLeft, écoute l'événement "click" et affiche un message dans la console quand on clique.
+	arrowLeft.addEventListener("click", () => {
+		console.log("j'ai cliqué sur la flèche de gauche");
+	});
+
+//  Idem pour arrowRight
+	arrowRight.addEventListener("click", () => {
+		console.log("j'ai cliqué sur la flèche de droite");
+	});
